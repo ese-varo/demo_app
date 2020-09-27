a3136acfd55facaba433c2c9ed8e11eba880ae7f
Alvaro Padilla <varoboss.ap@gmail.com>
2020-09-27 18:59:29 -0500

    Add views for employees

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,16 +1,14 @@
 class EmployeesController < ApplicationController
-  before_action :get_branch
-  before_action :set_employee, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @employees = @branch.employees
-  end
+  # before_action :get_branch, only: %i[show edit update destroy]
+  before_action :get_branch, except: :new
+  before_action :set_employee, only: %i[show edit update destroy]
 
   def show
   end
 
   def new
-    @employee = Employee.new
+    # @employee = Employee.new
+    @employee = current_user.branches.first.employees.build
   end
 
   def edit
@@ -18,11 +16,14 @@ class EmployeesController < ApplicationController
 
   def create
     # @employee = Employee.new(employee_params)
-    @employee = @branch.employees.build(branch_params)
+    # branch = current_user.branches.find_by(name: params[:branch])
+    # @employee.branch = Branch.find_by(name: params[:branch])
+    # @employee = Employee.new(employee_params)
+    
+    @employee = get_branch.employees.build(employee_params)
 
     respond_to do |format|
       if @employee.save
-        # format.html { redirect_to @employee, notice: 'Employee was successfully created.' }
         format.html { redirect_to branch_employees_path(@branch), notice: 'Employee was successfully created.' }
         format.json { render :show, status: :created, location: @employee }
       else
@@ -35,7 +36,6 @@ class EmployeesController < ApplicationController
   def update
     respond_to do |format|
       if @employee.update(employee_params)
-        # format.html { redirect_to @employee, notice: 'Employee was successfully updated.' }
         format.html { redirect_to branch_employee_path(@branch), notice: 'Employee was successfully updated.' }
         format.json { render :show, status: :ok, location: @employee }
       else
@@ -48,7 +48,6 @@ class EmployeesController < ApplicationController
   def destroy
     @employee.destroy
     respond_to do |format|
-      # format.html { redirect_to employees_url, notice: 'Employee was successfully destroyed.' }
       format.html { redirect_to branch_employees_path(@branch), notice: 'Employee was successfully destroyed.' }
       format.json { head :no_content }
     end
@@ -57,11 +56,12 @@ class EmployeesController < ApplicationController
   private
 
     def get_branch
-      @branch = Branch.find(params[:branch_id])
+      # @branch = Branch.find(params[:branch_id])
+      @branch = Branch.find_by(name: params[:branch])
     end
 
     def set_employee
-      @employee = @branch.employees.find(params[:id])
+      # @employee = @branch.employees.find(params[:id])
     end
 
     def employee_params
