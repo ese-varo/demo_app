37cc5c49f50b0bb5cedb0eee09340a452ed727ef
Alvaro Padilla <varoboss.ap@gmail.com>
2020-09-27 10:52:53 -0500

    Add Employee model validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,8 @@
 class Employee < ApplicationRecord
   belongs_to :branch
+  validates :first_name, :maiden_name, :last_name, :rfc, :branch, presence: true
+  validates :rfc, length: {
+    in: 12..13,
+    wrong_length: 'RFC should be 12/13 characters long'
+  }, uniqueness: { case_sensitive: false }
 end
