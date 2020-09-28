dd9d47d57b86d78540124724631cdff780ea2b74
Alvaro Padilla <varoboss.ap@gmail.com>
2020-09-28 18:57:37 -0500

    Refactor employee controller actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,7 +1,7 @@
 class EmployeesController < ApplicationController
-  # before_action :get_branch, only: %i[show edit update destroy]
-  before_action :get_branch, except: :new
-  before_action :set_employee, only: %i[show edit update destroy]
+  before_action :set_employee, only: %i[edit update destroy]
+  before_action :get_branch, only: %i[edit update destroy]
+
   def show
   end
 
@@ -16,10 +16,11 @@ class EmployeesController < ApplicationController
     @employee = Employee.new(employee_params)
     @employee.branch_id = @employee.company_branch.to_i
     # byebug
-    @employee.company_branch = Branch.find_by(id: @employee.branch_id)[:name]
+    branch = Branch.find_by(id: @employee.branch_id)
+    @employee.company_branch = branch.name
     respond_to do |format|
       if @employee.save
-        format.html { redirect_to branch_employees_path(@branch), notice: 'Employee was successfully created.' }
+        format.html { redirect_to edit_branch_path(branch), notice: 'Employee was successfully created.' }
         format.json { render :show, status: :created, location: @employee }
       else
         format.html { render :new }
@@ -31,7 +32,7 @@ class EmployeesController < ApplicationController
   def update
     respond_to do |format|
       if @employee.update(employee_params)
-        format.html { redirect_to branch_employee_path(@branch), notice: 'Employee was successfully updated.' }
+        format.html { redirect_to edit_branch_path(@branch), notice: 'Employee was successfully updated.' }
         format.json { render :show, status: :ok, location: @employee }
       else
         format.html { render :edit }
@@ -43,26 +44,22 @@ class EmployeesController < ApplicationController
   def destroy
     @employee.destroy
     respond_to do |format|
-      format.html { redirect_to branch_employees_path(@branch), notice: 'Employee was successfully destroyed.' }
+      format.html { redirect_to edit_branch_path(@branch), notice: 'Employee was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
   
   private
 
-    def get_company_branch
-      Branch.find_by(id: @employee.branch_id).name
+    def get_branch
+      @branch = Branch.find_by(id: @employee.branch_id)
     end
 
-    def get_branch
-      # @branch = Branch.find_by(id: get_branch_id)
+    def set_employee
+      @employee = Employee.find_by(id: params[:id])
     end
 
     def employee_params
       params.require(:employee).permit(:first_name, :maiden_name, :last_name, :rfc, :position, :company_branch)
-    end
-
-    def set_employee
-      # @employee = @branch.employees.find(params[:id])
     end
 end
